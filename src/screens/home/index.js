f1e1bf5ac5743a7361fbfa5aaf638f1d8de8b3db
John <john@Annalisas-Mac-mini.local>
2022-06-08 07:12:50 -0400

    add expand location and markers

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {
   StyleSheet,
   Text,
@@ -13,6 +13,7 @@ import tw from 'twrnc';
 import CheckBox from '@react-native-community/checkbox';
 import MapboxGL,{ Logger }  from "@rnmapbox/maps";
 import iconImage from '../../assets/images/markerIcon-100px.png';
+import ileOrleans from '../../assets/images/ile-orleans.png';
 MapboxGL.setAccessToken('pk.eyJ1Ijoiam9obmNoYWZpIiwiYSI6ImNsM3diN3ZqZTAwbmczamtpdm5pb3EzdmMifQ.QN4wp_RcSviV5SeqtgtnwQ');
 // remove warnings
 Logger.setLogCallback(log => {
@@ -36,7 +37,7 @@ LogBox.ignoreLogs([
 
 
 
-const CheckBoxContainer = ({closeModal}) =>{
+const CheckBoxContainer = ({closeModal, expandPlaces}) =>{
   const [isSelected, setSelection] = useState(false);
 
   const places= ['Museum', 'Restaurant', 'Cabanne a sucre', 'Hotels'];
@@ -51,6 +52,11 @@ const CheckBoxContainer = ({closeModal}) =>{
       temp.push(place);
     }
     setSelectedPlaces(temp);
+
+  }
+  const save = () => {
+    expandPlaces(selectedPlaces);
+    closeModal();
 
   }
   return (
@@ -69,48 +75,133 @@ const CheckBoxContainer = ({closeModal}) =>{
          )
        }
        <View style={tw`flex-row mt-2`}>
-        <TouchableOpacity onPress={closeModal} style={tw.style(" ml-2 self-start rounded w-12 h-6", {backgroundColor:'#2D5182'})}><Text style={tw`text-white text-center`}>Save</Text></TouchableOpacity>
-        <TouchableOpacity onPress={closeModal} style={tw.style("ml-24 rounded w-12 h-6 bg-red-500")}><Text style={tw`text-white text-center`}>Close</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => save()} style={tw.style(" ml-2 self-start rounded w-12 h-6", {backgroundColor:'#2D5182'})}><Text style={tw`text-white text-center`}>Save</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => closeModal()} style={tw.style("ml-24 rounded w-12 h-6 bg-red-500")}><Text style={tw`text-white text-center`}>Close</Text></TouchableOpacity>
       </View>
 
     </View>
      
   );
 } ;
+
+
+const ContentContainer = ({close})=> {
+  return(
+    <View style={tw`flex-col items-center`}>
+       <TouchableOpacity onPress={() => close()} style={tw.style("self-end ml-2 rounded-full w-4 h-4 mt-1 mr-2", {backgroundColor:'#2D5182'})}><Text style={tw`text-white text-center font-bold`}>X</Text></TouchableOpacity>
+       <Text style={tw`self-center font-bold text-black`}>Chocolaterie de l'Ile d'olréans</Text>
+        <Image
+          source={ileOrleans}
+          style={{
+            width: 200,
+            height: 100,
+            resizeMode: "cover",
+          }}
+        />
+      <Text style={tw`self-center text-black text-xs mx-3`}>Lorem Ipsum dolor sit amet, consecteteur adipiscing elit, sed do elusmod tempor incididunt
+      ut labore et dolore magna aliqua
+      </Text>
+
+      <TouchableOpacity onPress={() => console.log("yes")} style={tw.style(" ml-2 rounded w-24 h-6", {backgroundColor:'#2D5182'})}><Text style={tw`text-white text-center font-bold`}>Learn more</Text></TouchableOpacity>
+    </View>
+  )
+
+}
+
+
+
+
+
+
 const Home = () => {
-  const coordinates = [-75.7773873, 45.4315564];
-  const [isExpanded, setIsExpanded] = useState(false)
+  let thisCamera = useRef();
+  let thisMap = useRef();
+  //const coordinates = [-75.7773873, 45.4315564];
+  const [coordinates, setCoordinates] = useState([]);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isContentOpened, setIsContentOpened] = useState(false);
+  const [zoomLevel, setZoomLevel] = useState(4);
+  const [placesToExplor, setPlacesToExplor] = useState([]);
+  // const museumCoordinates = [[-77.7773873, 45.4315564], [-75.7773873, 44.4315564], [-78.7773873, 45.4315564], [-75.7773873, 42.4315564], [-75.7773873, 48.4315564]]
+  const museumCoordinates = [[-75.7773873, 44.4315564]];
+
+  const expandPlaces = (plcs) =>{
+    console.log(thisCamera);
+    setPlacesToExplor(plcs);
+    setZoomLevel(6);
+    thisCamera.fitBounds(museumCoordinates);
+  }
+
+  useEffect(()=>{
+    MapboxGL.locationManager.start();
+  }, [])
+
+  const onRegionDidChange = async () => {
+    const center = await thisMap.getCenter();
+    setCoordinates({ center });
+  }
+
+  const bounds = {
+    ne: [-70.7773873-2, 30.054738],
+    sw: [-70.760365, 40.947256],
+  };
+  
   return (
     <SafeAreaView style={styles.page}>
         <View style={styles.container}>
-          <MapboxGL.MapView style={styles.map}>
+          <MapboxGL.MapView style={styles.map} ref={(c) => (thisMap = c)} onRegionDidChange={onRegionDidChange}>
             <MapboxGL.Camera
-              zoomLevel={4}
+              zoomLevel={zoomLevel}
               animationMode={'flyTo'}
               animationDuration={500}
               followUserLocation={false}
               followUserMode={'normal'}
-              centerCoordinate={coordinates}
+              centerCoordinate={[-77.7773873, 45.4315564]}
+              ref={(ref) => (thisCamera = ref)}
             />
-            <MapboxGL.MarkerView id={"marker"}  coordinate={coordinates} >
-            <View style={styles.markerContainer}>
-                <TouchableOpacity onPress={()=>setIsExpanded(true)}>
-                  <Image
-                    source={iconImage}
-                    style={{
-                      width: 20,
-                      height: 30,
-                      resizeMode: "cover",
-                    }}
-                  />
-                </TouchableOpacity>
-                {isExpanded ?
-                <View style={styles.textContainer}>
-                 <CheckBoxContainer closeModal={() => setIsExpanded(false)}/> 
-                </View>
-                : null}  
-            </View>
+            <MapboxGL.MarkerView id={"marker"}  coordinate={[-77.7773873, 45.4315564]} >
+              <View style={styles.markerContainer}>
+                  <TouchableOpacity onPress={()=>setIsExpanded(true)}>
+                    <Image
+                      source={iconImage}
+                      style={{
+                        width: 20,
+                        height: 30,
+                        resizeMode: "cover",
+                      }}
+                    />
+                  </TouchableOpacity>
+                  {isExpanded ?
+                  <View style={styles.textContainer}>
+                  <CheckBoxContainer closeModal={() => setIsExpanded(false)} expandPlaces={expandPlaces} /> 
+                  </View>
+                  : null}  
+              </View>
             </MapboxGL.MarkerView>
+            {
+             placesToExplor.includes('Museum')&&(
+              museumCoordinates.map((coord, index) =>
+              <MapboxGL.MarkerView key={`indexmarker-${index}`} id={`indexmarker-${index}`}  coordinate={coord} >
+              <View style={styles.markerContainer}>
+                  <TouchableOpacity onPress={()=> setIsContentOpened(true)}>
+                    <Image
+                      source={iconImage}
+                      style={{
+                        width: 20,
+                        height: 30,
+                        resizeMode: "cover",
+                      }}
+                    />
+                  </TouchableOpacity>
+                  {isContentOpened ?
+                  <View style={styles.textContainer}>
+                    <ContentContainer close={() => setIsContentOpened(false)} /> 
+                  </View>
+                  : null}  
+              </View>
+              </MapboxGL.MarkerView> )
+              )
+            }
           </MapboxGL.MapView>
         </View>
     </SafeAreaView>
